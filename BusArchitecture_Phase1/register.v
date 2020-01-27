0d4de6fa911e51f032bd2bef5e5f982eb180c67d
Daniyal <mdaniyalmaniar@hotmail.com>
2020-01-26 20:33:58 -0500

    Added Registor Select

--- BusArchitecture_Phase1/register.v
+++ BusArchitecture_Phase1/register.v
@@ -1,23 +1,18 @@
-module register(clk, clear, loadEnable, inputD, outputQ);
+// Module for single register
+module register #(parameter BITS = 32)(
+	input clk, input clear, input loadEnable, 
+	input [BITS-1:0] inputD, 
+	output reg [BITS-1:0] outputQ);
 
-input [31:0] inputD;
-input clk, clear, loadEnable;
-
-output reg [31:0] outputQ;
-
-// should we have this?
+// To check whether clock or clear is selected
 wire sen;
 assign sen = clk || clear;
 
+// At ever positive edge do this
 always @ (posedge sen)
-begin
+	// If clear else put value
 	if (clear == 1)
-	begin
-		outputQ <= 0;
-	end
+		outputQ <= {BITS{1'b0}};
 	else if (loadEnable == 1)
-	begin
 		outputQ <= inputD;
-	end
-end
 endmodule
