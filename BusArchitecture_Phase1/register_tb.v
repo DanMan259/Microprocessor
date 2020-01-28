eef0b644231800154b8137d4336010371560a782
Daniyal <mdaniyalmaniar@hotmail.com>
2020-01-28 12:27:08 -0500

    Updated testbench

--- BusArchitecture_Phase1/register_tb.v
+++ BusArchitecture_Phase1/register_tb.v
@@ -5,29 +5,40 @@ module register_tb;
 reg [31:0] input_D;
 reg clk;
 reg clear;
-reg loadEnable;
+//reg loadEnable;
+wire loadEnable;
+assign loadEnable = clk;
 wire [31:0] outputQ;
 
-register reg_32(clk, clear, clk, input_D, outputQ);
+register reg_32(clk, clear, loadEnable, input_D, outputQ);
 
 
 
-initial 
-	begin
-		// Generate clock signal
+initial begin
 		clk = 0;
+		clear = 0;
+		input_D = 32'b0;
 		forever #10 clk = ~clk;
 end
 
-initial
-	begin
-		@ (posedge clk);
-			input_D <= 32'b0;
-		@ (posedge clk);
-			input_D <= {32{1'b1}};
-		@ (posedge clk);
-			input_D <= 32'b0;
-end
 
+initial begin
+	@ (posedge clk)
+		input_D = ~input_D;
+	@ (posedge clk)
+		input_D = ~input_D;
+	@ (posedge clk)
+		clear = 1;
+		input_D = ~input_D;
+	@ (posedge clk)
+		input_D = ~input_D;
+	@ (posedge clk)
+		clear = 0;
+		input_D = ~input_D;
+	@ (posedge clk)
+		input_D = ~input_D;
+	@ (posedge clk)
+		input_D = ~input_D;
+	end
 endmodule
 	
