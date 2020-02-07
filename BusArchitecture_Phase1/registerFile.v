ccf2f4580c73ebebd23dfa05e8875d2f3935454f
Hermann Krohn <16hhk@queensu.ca>
2020-02-06 22:35:24 -0500

    Adds the register file test bench. Also, register file now initializes registers

--- BusArchitecture_Phase1/registerFile.v
+++ BusArchitecture_Phase1/registerFile.v
@@ -1,8 +1,14 @@
 // Register File
-module registerFile #(parameter BITS = 32, REGISTERS = 24)(
-			output reg [BITS * REGISTERS - 1 : 0] registerStream
+module registerFile #(parameter BITS = 32, REGISTERS = 16)(
+			input [BITS-1:0] busMuxOut,
+			input clk,
+			input [REGISTERS-1:0] clr, loadEnable,
+			output [BITS * REGISTERS - 1 : 0] registerStream
 );
-	always @* begin
-		registerStream <= {BITS*REGISTERS{1'b0}};
-	end
+	generate
+	genvar i;
+		for (i = 0; i < REGISTERS; i = i + 1) begin: gen_registers
+			register generalRegister(clk, clr[i], loadEnable[i], busMuxOut, registerStream[(i+1) * BITS - 1 : i*BITS]);
+		end
+	endgenerate	
 endmodule
--- BusArchitecture_Phase1/registerSelect.v
+++ BusArchitecture_Phase1/registerSelect.v
@@ -1,17 +1,17 @@
 // Module for register file
 
-module registerSelect #(parameter BITS = 32, REGISTERS = 24)(
-			input [BITS * REGISTERS - 1 : 0] registerIns,
+module registerSelect #(parameter BITS = 32, REGISTERS = 16)(
+			input [BITS * REGISTERS - 1 : 0] registerStream,
 			input [REGISTERS - 1 : 0] registerSelect,
 			output reg [BITS - 1 : 0] busMuxOut
 );
 	generate
-      genvar i;
-		for (i = 0; i < REGISTERS; i = i + 1) begin : register_selector
-			always @* begin
-				if (registerSelect[i] == 1)
-					busMuxOut <= registerIns[(i+1)*BITS-1: i*BITS];
-			end
+	genvar i;
+	for (i = 0; i < REGISTERS; i = i + 1) begin : register_selector
+		always @* begin
+			if (registerSelect[i] == 1)
+				busMuxOut <= registerStream[(i+1)*BITS-1: i*BITS];
 		end
+	end
 	endgenerate
 endmodule
