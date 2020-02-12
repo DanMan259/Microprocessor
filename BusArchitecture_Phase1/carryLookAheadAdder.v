8e6f197c9f7590fa3b603c4f43cde5dbe3cfc99e
Hermann Krohn <16hhk@queensu.ca>
2020-02-12 17:41:26 -0500

    Untested version of carryLookAheadAdder

--- BusArchitecture_Phase1/carryLookAheadAdder.v
+++ BusArchitecture_Phase1/carryLookAheadAdder.v
@@ -12,7 +12,7 @@ module carry_look_ahead_adder #(parameter BITS=32)(
 	// Create the full adders
 	genvar i;
 	generate
-		for(i=0; i<BITS; i=i+1) begin
+		for(i=0; i<BITS; i=i+1) begin : genFullAdders
 			fulladder_cla full_adder_inst(
 				.summand1(summand1_32_bits[i]),
 				.summand2(summand2_32_bits[i]),
@@ -26,6 +26,15 @@ module carry_look_ahead_adder #(parameter BITS=32)(
 	// Generate the Generate terms and Propagate terms and Carry Terms
 	genvar j;
 	generate
-		for(j=0; j<BITS; j=j+1)
-			begin
-				// FINISH THIS
+		for(j=0; j<BITS; j=j+1) begin : genTerms
+				assign w_G[j] = summand1_32_bits[j] & summand2_32_bits[j];
+				assign w_P[j] = summand1_32_bits | summand2_32_bits[j];
+				assign w_C[j+1] = w_G[j] |(w_P[j] & w_C[j]);
+			end
+	endgenerate
+	
+	assign w_C[0] = 1'b0; // no carry input to the first full adder
+	
+	assign outputSum = {w_C[BITS], w_Sum}; // concatenation
+	
+endmodule
