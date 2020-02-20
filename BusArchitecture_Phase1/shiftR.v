6c13fa1a02adaae291b4dbc4fc79530ef71ed433
Daniyal <mdaniyalmaniar@hotmail.com>
2020-02-20 17:19:49 -0500

    Changes to shift

--- BusArchitecture_Phase1/shiftR.v
+++ BusArchitecture_Phase1/shiftR.v
@@ -5,5 +5,5 @@ module shiftR #(parameter BITS=32)(
 	input [BITS-1:0] shiftAmount,
 	output wire [BITS-1:0] out
 );
-	assign out = (in >> (shiftAmount % BITS));
+	assign out = (in >> shiftAmount);
 endmodule
